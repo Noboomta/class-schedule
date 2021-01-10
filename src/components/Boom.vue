1e7a10f0d1e251334a3733d6c4c3edacfa0a7b34
noboomta <puvana.jp36@gmail.com>
2021-01-10 23:26:47 +0700

    add Boom

---
+++ src/components/Boom.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="boom">
+        <h1>{{ age }}{{ int }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "boom",
+    props: {
+        age: int,
+        nickname: String
+    }
+}
+</script>
